c07cb7ac6ed1f2572661f283678cbb015f65398a
Zemin Xu <zemin252@qq.com>
2019-06-18 11:02:29 +0800

    添加项目文件。

---
+++ CBQ/LoadIndex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CBQ
+{
+    public partial class LoadIndex : Form
+    {
+        public LoadIndex()
+        {
+            InitializeComponent();
+
+            if (!this.txtValidCode.Text.Equals(_validCode.CheckCode))
+            {
+
+                MessageBox.Show(" 请输入正确的验证码!", this.Text);
+
+                this.txtValidCode.Focus();
+
+                return;
+
+            }
+            picValidCode.Image = Bitmap.FromStream(ValidCode.CreateCheckCodeImage());
+
+        }
+
+
+    }
+}
